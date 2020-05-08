44c4f31e802d7a9205263323c6ec2b0d26b652ea
Sahand Masoleh <sahand.masoleh@gmail.com>
2020-05-09 01:47:17 +0800

    Updates

---
+++ precache-manifest.2278f35e279eb38c5a1345d4dc198bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1021cf9b011d3eb7d5ab581f7fa4a36f",
+    "url": "/project-js-calculator/index.html"
+  },
+  {
+    "revision": "b803c4b653868a9d67be",
+    "url": "/project-js-calculator/static/css/main.a64508d1.chunk.css"
+  },
+  {
+    "revision": "789ed5a776cbfe814740",
+    "url": "/project-js-calculator/static/js/2.5d80f041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-js-calculator/static/js/2.5d80f041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b803c4b653868a9d67be",
+    "url": "/project-js-calculator/static/js/main.ae58c48e.chunk.js"
+  },
+  {
+    "revision": "3a96088420475a616cd7",
+    "url": "/project-js-calculator/static/js/runtime-main.77321d80.js"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/project-js-calculator/static/media/DS-DIGI.63f874d1.TTF"
+  }
+]);
